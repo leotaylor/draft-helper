7640a110dd34cc98a3ee9302ae0a54d9429637c3
Leo Taylor <leotaylor723@gmail.com>
2018-07-18 11:07:26 -0500

    rb and wr print

--- src/components/PlayerPositions/WR/Wr.js
+++ src/components/PlayerPositions/WR/Wr.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 import './Wr.css';
 
+import footballNerdRequest from '../../../footballApiRequests/footballNerdRequest';
+
 class WR extends React.Component {
+  state = {
+    players: [],
+  }
+
+  componentDidMount () {
+    footballNerdRequest.getRankings()
+      .then((players) => {
+        this.setState({players: players.data.DraftRankings});
+      })
+      .catch((err) => {
+        console.error('error with RB get requests', err);
+      });
+  }
+
   render () {
+    const playerComponent = this.state.players.map((player) => {
+      if (player.position === 'WR') {
+        return (
+          <li className='row' key={player.playerId}>
+            <p>{player.displayName} {player.team}</p>
+            <button className="btn btn-danger">Drafted</button>
+            <button className="btn btn-success">Mine</button>
+          </li>
+        );
+      }
+    });
     return (
       <div className="WR">
         <h1>Wide Receiver</h1>
+        {playerComponent}
       </div>
     );
   }
