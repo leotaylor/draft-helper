f4d6b1eafb7a3918552df56dc1f5e15f6c1f479b
Leo Taylor <leotaylor723@gmail.com>
2018-07-17 20:49:08 -0500

    got data and bootstrap grid

---
+++ src/footballApiRequests/footballNerdRequest.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import constants from '../constants';
+
+const getRankings = () => {
+  try {
+    return axios.get(`https://draft-helper.herokuapp.com/api/fantasyfootballnerd/service/draft-rankings/json/${constants.footballNerd.apiKey}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default {getRankings};
